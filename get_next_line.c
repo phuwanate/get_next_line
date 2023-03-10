b216f8e66ed21b5e471da4e995f8d1f91dbf4206
Phuwanate Lertsiriyothin <plertsir@z1t6c3.42bangkok.com>
2023-03-10 12:01:31 +0700

    100323

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: plertsir <plertsir@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:06:08 by plertsir          #+#    #+#             */
-/*   Updated: 2023/03/10 02:42:47 by first            ###   ########.fr       */
+/*   Updated: 2023/03/10 11:56:09 by plertsir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,49 @@
 char	*read_file(int fd, char *buff, char *stash)
 {
 	int		read_status;
+	char	*stash_temp;
 
 	read_status = 1;
 	while (read_status != 0)
 	{
+		read_status = read(fd, buff, BUFFER_SIZE);
 		if (read_status == -1)
 			return (NULL);
 		if (read_status == 0)
 			break ;
+		buff[read_status] = '\0';
 		if (!stash)
 			stash = ft_strdup("");
-		read_status = read(fd, buff, BUFFER_SIZE);
-		buff[BUFFER_SIZE + 1] = '\0';
-		stash = ft_strjoin(stash, buff);
-		if (ft_strchr(stash, '\n') != NULL)
+		stash_temp = stash;
+		stash = ft_strjoin(stash_temp, buff);
+		free(stash_temp);
+		stash_temp = NULL;
+		if (ft_strchr(buff, '\n'))
 			break ;
 	}
 	return (stash);
 }
 
-char	*ft_clean(char *stash)
+char	*ft_clean(char *line)
 {
 	int		i;
 	size_t	len_stash;
-	char	*line;
+	char	*stash;
 
-	len_stash = ft_strlen(stash);
+	len_stash = ft_strlen(line);
 	i = 0;
-	while (stash[i] != '\n' && stash[i] != '\0')
+	while (line[i] != '\n' && line[i] != '\0')
 		i++;
-	line = ft_substr(stash, 0, i + 1);
-	while ((stash[len_stash] != '\n') && (len_stash != 0))
-		len_stash--;
-	stash = ft_strdup(&stash[len_stash]);
-	return (line);
+	if (line[i] == '\0' || line[1] == '\0')
+		return (NULL);
+	stash = ft_substr(line, i + 1, len_stash - i);
+	if (*stash == '\0')
+	{
+		free(stash);
+		stash = NULL;
+	}
+	line[i + 1] = '\0';
+	return (stash);
 }
 
 char	*get_next_line(int fd)
@@ -62,11 +71,11 @@ char	*get_next_line(int fd)
 	buff = (char *)malloc((BUFFER_SIZE + 1) + sizeof(char));
 	if (!buff)
 		return (NULL);
-	stash = read_file(fd, buff, stash);
+	line = read_file(fd, buff, stash);
 	free(buff);
 	buff = NULL;
-	if (!stash)
+	if (!line)
 		return (NULL);
-	line = ft_clean(stash);
+	stash = ft_clean(line);
 	return (line);
 }
